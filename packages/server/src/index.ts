a03cbf1cfd019204cdce0fca9b0b5da1993406f0
daviromao <davi.romao0210@gmail.com>
2022-11-08 00:13:56 -0300

    feat(server): create auth controller

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,7 @@
 import App from "./app";
+import AuthRoute from "./routers/auth.route";
 import IndexRoute from "./routers/index.route";
 
-const app = new App([new IndexRoute()]);
+const app = new App([new IndexRoute(), new AuthRoute()]);
 
 app.listen();
