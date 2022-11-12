392ab0e4497366cb75e298537b78a342392fe9f2
daviromao <davi.romao0210@gmail.com>
2022-11-11 23:31:12 -0300

    feat: improve types of vacancy dto

--- packages/server/src/dtos/vacancy.dto.ts
+++ packages/server/src/dtos/vacancy.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsDecimal } from "class-validator";
+import { IsString, IsDecimal, IsOptional, IsIn } from "class-validator";
 import { WorkRegime, WorkLoad, WorkType, ExperienceLevel } from "@prisma/client";
 
 class CreateVacancyDto {
@@ -9,18 +9,23 @@ class CreateVacancyDto {
   public description!: string;
 
   @IsDecimal()
+  @IsOptional()
   public salary?: number;
 
   @IsString()
+  @IsIn(Object.values(WorkRegime))
   public workRegime!: WorkRegime;
 
   @IsString()
+  @IsIn(Object.values(WorkLoad))
   public workLoad!: WorkLoad;
 
   @IsString()
+  @IsIn(Object.values(WorkType))
   public workType!: WorkType;
 
   @IsString()
+  @IsIn(Object.values(ExperienceLevel))
   public experienceLevel!: ExperienceLevel;
 }
 
