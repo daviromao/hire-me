6a7dec7e92fd1eebcc3795a6b236107eb75442dd
daviromao <davi.romao0210@gmail.com>
2022-11-11 23:31:30 -0300

    feat: improve validations in controllers

--- packages/server/src/controllers/vacancy.controller.ts
+++ packages/server/src/controllers/vacancy.controller.ts
@@ -1,15 +1,16 @@
-import { Company, Vacancy } from "@prisma/client";
+import { Company } from "@prisma/client";
 import type { Request, Response, NextFunction } from "express";
 import CreateVacancyDto from "../dtos/vacancy.dto";
 import HttpException from "../exceptions/HttpException";
 import VacancyService from "../services/vacancy.service";
+import { validate } from "../utils/validator";
 
 class VacancyController {
   public vacancyService = new VacancyService();
 
   public index = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      const vacancies: Vacancy[] = await this.vacancyService.findAll();
+      const vacancies = await this.vacancyService.findAll();
       res.status(200).json({ data: vacancies });
     } catch (error) {
       next(error);
@@ -18,8 +19,8 @@ class VacancyController {
 
   public show = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      const vacancyId: string = req.params.id;
-      const vacancy: Vacancy = await this.vacancyService.findById(vacancyId);
+      const vacancyId = req.params.id;
+      const vacancy = await this.vacancyService.findById(vacancyId);
       res.status(200).json({ data: vacancy });
     } catch (error) {
       next(error);
@@ -28,8 +29,8 @@ class VacancyController {
 
   public store = async (user: Company, req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      const vacancyData: CreateVacancyDto = req.body;
-      const createVacancyData: Vacancy = await this.vacancyService.create(vacancyData, user);
+      const vacancyData = await validate(CreateVacancyDto, req.body);
+      const createVacancyData = await this.vacancyService.create(vacancyData, user);
       res.status(201).json({ data: createVacancyData, message: "created" });
     } catch (error) {
       next(error);
@@ -38,13 +39,13 @@ class VacancyController {
 
   public update = async (user: Company, req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      const vacancyId: string = req.params.id;
-      const vacancyData: CreateVacancyDto = req.body;
-      const vacancy: Vacancy = await this.vacancyService.findById(vacancyId);
+      const vacancyId = req.params.id;
+      const vacancyData = await validate(CreateVacancyDto, req.body, { skipMissingProperties: true });
+      const vacancy = await this.vacancyService.findById(vacancyId);
 
       if (user.id !== vacancy.companyId) throw new HttpException(401, "Unauthorized");
 
-      const updateVacancyData: Vacancy = await this.vacancyService.update(vacancyId, vacancyData);
+      const updateVacancyData = await this.vacancyService.update(vacancyId, vacancyData);
 
       res.status(200).json({ data: updateVacancyData, message: "updated" });
     } catch (error) {
@@ -54,8 +55,8 @@ class VacancyController {
 
   public delete = async (user: Company, req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      const vacancyId: string = req.params.id;
-      const vacancy: Vacancy = await this.vacancyService.findById(vacancyId);
+      const vacancyId = req.params.id;
+      const vacancy = await this.vacancyService.findById(vacancyId);
 
       if (user.id !== vacancy.companyId) throw new HttpException(401, "Unauthorized");
 
